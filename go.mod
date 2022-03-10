d455cbcf37fbb806b555e834481f0f4ad0ec017f
Eric Fialkowski <eric.fialkowski@gmail.com>
2022-03-09 19:47:58 -0700

    added option to generate cuids

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module uuid
 
 go 1.17
 
-require github.com/pborman/uuid v1.2.1
+require github.com/lucsky/cuid v1.2.1
 
-require github.com/google/uuid v1.3.0 // indirect
+require github.com/google/uuid v1.3.0
--- main.go
+++ main.go
@@ -1,42 +1,85 @@
 package main
 
 import (
+	"crypto/rand"
 	"flag"
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/lucsky/cuid"
 	"os"
 	"strings"
 )
 
 func main() {
 	var dash bool
-	var count int
 	var version int
+
+	var slug bool
+	var crypt bool
+
+	var genCuid bool
+
+	var count int
 	var sep string
+
+	flag.BoolVar(&slug, "slug", false, "Generate a cuid slug instead of uuid")
+	flag.BoolVar(&crypt, "crypt", false, "Generate cryptographic random cuid instead of uuid")
 	flag.BoolVar(&dash, "d", true, "Print uuid with dashes")
-	flag.IntVar(&count, "n", 1, "Number of uuids generate")
 	flag.IntVar(&version, "v", 4, "Version of UUID to generate (1 or 4)")
-	flag.StringVar(&sep, "s", "\n", "Separator character for generating multiple uuids")
+	flag.BoolVar(&genCuid, "cuid", false, "Generate cuid instead of uuid")
+	flag.IntVar(&count, "n", 1, "Number of uuids generate")
+	flag.StringVar(&sep, "sep", "\n", "Separator character to use when generating multiples")
 	flag.Parse()
 
 	for i := 0; i < count; i++ {
-		var u uuid.UUID
-		if version == 1 {
-			u = uuid.Must(uuid.NewUUID())
-		} else if version == 4 {
-			u = uuid.New()
+		var u string
+		if genCuid || slug || crypt {
+			u = createCuid(slug, crypt)
 		} else {
-			fmt.Println("Version must be either 1 or 4")
-			os.Exit(1)
+			u = createUUID(dash, version)
 		}
+
 		lastChar := sep
 		if i == count-1 {
 			lastChar = ""
 		}
-		if dash {
-			fmt.Printf("%v%s", u, lastChar)
+
+		fmt.Printf("%s%s", u, lastChar)
+	}
+}
+
+func createUUID(dash bool, version int) string {
+	var u uuid.UUID
+	if version == 1 {
+		u = uuid.Must(uuid.NewUUID())
+	} else if version == 4 {
+		u = uuid.New()
+	} else {
+		fmt.Println("Version must be either 1 or 4")
+		os.Exit(2)
+	}
+	if dash {
+		return fmt.Sprintf("%v", u)
+	} else {
+		return fmt.Sprintf("%s", strings.Replace(u.String(), "-", "", -1))
+	}
+}
+
+func createCuid(slug bool, crypt bool) string {
+	var c string
+	if crypt {
+		var err error
+		c, err = cuid.NewCrypto(rand.Reader)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(3)
+		}
+	} else {
+		if slug {
+			c = cuid.Slug()
 		} else {
-			fmt.Printf("%s%s", strings.Replace(u.String(), "-", "", -1), lastChar)
+			c = cuid.New()
 		}
 	}
+	return c
 }
